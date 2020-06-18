89c21164af125c130faabb2ef1ee367f5c83c6e1
Thomas Buckley <tbuckley@tabwebsites.com>
2020-06-18 15:59:31 -0400

    added boilerplate command structure

---
+++ commands/args-info.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'args-info',
+    args: true,
+    usage: '<user> <role>',
+    description: 'Information about the arguments provided.',
+    execute(message, args) {
+        if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+
+        message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`);
+    },
+};
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const { prefix, token } = require('./config.json');
 
-const { prefix, token } = require("./config.json");
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+function getUserFromMention(mention) {
+    // The id is the first and only match found by the RegEx.
+    const matches = mention.match(/^<@!?(\d+)>$/);
+
+    // If supplied variable was not a mention, matches will be null instead of an array.
+    if (!matches) return;
+
+    // However the first element in the matches array will be the entire mention, not just the ID,
+    // so use index 1.
+    const id = matches[1];
+
+    return client.users.cache.get(id);
+}
+
+
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -11,12 +36,36 @@ client.on('message', message => {
     if (message.author.bot) return;
 
     console.log(message.content);
-
-    if (message.content === `${prefix}ping`) {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
+    if (message.content.startsWith(prefix)) {
+        var args = message.content.slice(prefix.length).split(' ');
+    } else if (message.content.startsWith('++') || message.content.startsWith('--')) {
+        var args = message.content.slice(2).split(' ');
+    } else {
+        return;
     }
 
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be : \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 
 client.login(token);
