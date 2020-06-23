e7f0f50b7c3a97006001264a93489df9a203d562
Thomas Buckley <tbuckley@tabwebsites.com>
2020-06-23 19:44:32 -0400

    added function to give rep to a user and store record in a retrievable format in sql

--- index.js
+++ index.js
@@ -1,70 +1,146 @@
-const fs = require('fs');
 const Discord = require('discord.js');
+const Sequelize = require('sequelize');
 const { prefix, token } = require('./config.json');
+const uuidv4 = require("uuid/v4")
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const PREFIX = '!';
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+})
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
-
-function getUserFromMention(mention) {
-    // The id is the first and only match found by the RegEx.
-    const matches = mention.match(/^<@!?(\d+)>$/);
-
-    // If supplied variable was not a mention, matches will be null instead of an array.
-    if (!matches) return;
-
-    // However the first element in the matches array will be the entire mention, not just the ID,
-    // so use index 1.
-    const id = matches[1];
-
-    return client.users.cache.get(id);
-}
-
-
+/*
+ equivalent to: CREATE TABLE tags (
+ name VARCHAR(255),
+ description TEXT,
+ username VARCHAR(255),
+ usage INT
+ );
+ */
+const Reputation = sequelize.define('reputation', {
+    guild_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    user_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    rep_given_by: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    rep_id: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+})
 
 client.once('ready', () => {
-    console.log('Ready!');
+    Reputation.sync();
 });
 
-client.on('message', message => {
-    if (message.author.bot) return;
+client.on('message', async message => {
+    if (message.content.startsWith(PREFIX)) {
+        const input = message.content.slice(PREFIX.length).split(' ');
+        const command = input.shift();
+        const commandArgs = input.join(' ');
 
-    console.log(message.content);
-    if (message.content.startsWith(prefix)) {
-        var args = message.content.slice(prefix.length).split(' ');
-    } else if (message.content.startsWith('++') || message.content.startsWith('--')) {
-        var args = message.content.slice(2).split(' ');
-    } else {
-        return;
-    }
+        if (command === 'addrep' && (message.mentions.users.size !== 0)) {
+            if (!message.mentions.users.size) {
+                return message.reply('you need to tag a user in order to give reputation!');
+            }
 
-    const commandName = args.shift().toLowerCase();
+            const splitArgs = commandArgs.split(' ');
+            const taggedUser = message.mentions.users.first();
+            const tagDescription = splitArgs.join(' ');
 
-    if (!client.commands.has(commandName)) return;
+            if (!tagDescription.length) {
+                return message.reply('you need to provide a reason for adding reputation!');
+            }
 
-    const command = client.commands.get(commandName);
+            try {
+                // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+                const reputation = await Reputation.create({
+                    guild_id: message.guild.id,
+                    user_id: taggedUser.id,
+                    user_name: taggedUser.tag,
+                    rep_given_by: message.author.tag,
+                    description: tagDescription,
+                    rep_id: uuidv4()
+                });
+                // check here if a user can rank up
+                return message.reply(`Rep added to ${taggedUser.tag} successfully.`);
+            }
+            catch (e) {
+                if (e.name === 'SequelizeUniqueConstraintError') {
+                    return message.reply('That rep UUID already exists.');
+                }
+                return message.reply('Something went wrong with adding reputation.');
+            }
 
-    if (command.args && !args.length) {
-        let reply = `You didn't provide any arguments, ${message.author}!`;
+        } else if (command === 'tag') {
+            const tagName = commandArgs;
 
-        if (command.usage) {
-            reply += `\nThe proper usage would be : \`${prefix}${command.name} ${command.usage}\``;
+            // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
+            const tag = await Tags.findOne({ where: { name: tagName } });
+            if (tag) {
+                // equivalent to: UPDATE tags SET usage_count = usage_count + 1 WHERE name = 'tagName';
+                tag.increment('usage_count');
+                return message.channel.send(tag.get('description'));
+            }
+            return message.reply(`Could not find tag: ${tagName}`);
+
+        } else if (command === 'edittag') {
+            const splitArgs = commandArgs.split(' ');
+            const tagName = splitArgs.shift();
+            const tagDescription = splitArgs.join(' ');
+
+            // equivalent to: UPDATE tags (descrption) values (?) WHERE name='?';
+            const affectedRows = await Tags.update({ description: tagDescription }, { where: { name: tagName } });
+            if (affectedRows > 0) {
+                return message.reply(`Tag ${tagName} was edited.`);
+            }
+            return message.reply(`Could not find a tag with name ${tagName}.`);
+
+        } else if (command === 'taginfo') {
+            const tagName = commandArgs;
+
+            // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
+            const tag = await Tags.findOne({ where: { name: tagName } });
+            if (tag) {
+                return message.channel.send(`${tagName} was created by ${tag.username} at ${tag.createdAt} and has been used ${tag.usage_count} times.`);
+            }
+            return message.reply(`Could not find tag: ${tagName}`);
+
+        } else if (command === 'showtags') {
+            // equivalent to: SELECT name FROM tags;
+            const tagList = await Tags.findAll({ attributes: ['name'] });
+            const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
+            return message.channel.send(`List of tags: ${tagString}`);
+
+        } else if (command === 'removetag') {
+            const tagName = commandArgs;
+            // equivalent to: DELETE from tags WHERE name = ?;
+            const rowCount = await Tags.destroy({ where: { name: tagName } });
+            if (!rowCount) return message.reply('That tag did not exist.');
+
+            return message.reply('Tag deleted.');
+
         }
-
-        return message.channel.send(reply);
-    }
-
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
     }
 });
 
