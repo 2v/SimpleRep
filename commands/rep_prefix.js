244c5b56fda09a845e6bbcb21a100286b81272c1
Thomas Buckley <tbuckley@tabwebsites.com>
2020-07-01 18:46:44 -0400

    fixed bug in prefix code

--- commands/rep_prefix.js
+++ commands/rep_prefix.js
@@ -10,8 +10,8 @@ module.exports = {
     async execute(message, args) {
         let formatted_prefix = String(args[0]);
 
-        if (formatted_prefix.length > 3) {
-            return message.reply(`\`${ formatted_prefix }\` is too long to be set as a prefix.`);
+        if (formatted_prefix.length > 3 || formatted_prefix.includes('\`')) {
+            return message.reply(`\`${ formatted_prefix }\` cannot be used as a prefix.`);
         }
 
         const prefix_entry = await Prefix.findOne({ where: { guild_id: message.guild.id } });
