6e41ab810af2098de1663d9bf62f81730d710065
Thomas Buckley <tbuckley@tabwebsites.com>
2020-07-03 23:07:55 -0400

    added model for cooldowns based on role

---
+++ models/RepCooldowns.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('repcooldowns', {
+        guild_id: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        trader_cooldown: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        reputable_cooldown: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        trusted_cooldown: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        timestamps: false,
+    })
+};
